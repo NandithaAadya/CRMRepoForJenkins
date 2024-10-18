99ee065e003659003274cc6431ee54e202ca02c3
NANAND <NANAND@INBG-3XDNM33.commscope.com>
2024-10-18 21:35:08 +0530

    First Commit Jenkins Nanditha

---
+++ ForJenkins/src/test/java/campaignModule/ToCreateCampainTest.java
@@ -0,0 +1,13 @@
+package campaignModule;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToCreateCampainTest {
+	@Test
+	public void createCampaign() {
+		Reporter.log("Created CampaignSuccessfully",true);
+		
+	}
+
+}
---
+++ ForJenkins/src/test/java/contactModule/ToCreateContactTest.java
@@ -0,0 +1,13 @@
+package contactModule;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToCreateContactTest{
+	@Test
+	public void createContact() {
+		Reporter.log("Created Contact Successfully",true);
+		
+	}
+
+}
